48f7d9c53f7e9fc28005c90cab3ce42785d5bf6d
Abdul Momin <abdulmomin114477@gmail.com>
2020-11-30 22:12:42 +0600

    all

---
+++ quote-generator/app.js
@@ -0,0 +1,7 @@
+"use strict";
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
