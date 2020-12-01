fdea0f1c5aa1e3866ac55ab09ded77a048de86bb
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-01 08:03:58 +0600

    all

--- picture-in-picture/app.js
+++ picture-in-picture/app.js
@@ -2,5 +2,13 @@
 // dom elements
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
+// Workaround the TypeScript Bug of not having getDisplayMedia
+// on type MediaDevices
+const mediaDevices = navigator.mediaDevices;
 // functions
+const askForScreen = async () => {
+    const media = await mediaDevices.getDisplayMedia();
+    console.log(media);
+};
+askForScreen();
 // event listeners
--- picture-in-picture/app.ts
+++ picture-in-picture/app.ts
@@ -1,7 +1,18 @@
 // dom elements
-const videoElement = document.getElementById('video');
-const button = document.getElementById('button');
+const videoElement = document.getElementById('video') as HTMLVideoElement;
+const button = document.getElementById('button') as HTMLButtonElement;
+
+// Workaround the TypeScript Bug of not having getDisplayMedia
+// on type MediaDevices
+const mediaDevices = navigator.mediaDevices as any;
 
 // functions
+const askForScreen = async () => {
+  const media = await mediaDevices.getDisplayMedia();
+
+  console.log(media);
+};
+
+askForScreen();
 
 // event listeners
