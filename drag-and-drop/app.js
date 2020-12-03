e74f4e0a1ac57cec231e6099e85c1b48ab9c26e2
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-03 19:29:18 +0600

    all

---
+++ drag-and-drop/app.js
@@ -0,0 +1,17 @@
+"use strict";
+// dom elements
+const elements = {
+    addBtns: document.querySelectorAll('.add-btn:not(.solid)'),
+    saveItemBtns: document.querySelectorAll('.solid'),
+    addItemContainers: document.querySelectorAll('.add-container'),
+    addItems: document.querySelectorAll('.add-item'),
+    // Item Lists
+    listColumns: document.querySelectorAll('.drag-item-list'),
+    backlogListEl: document.getElementById('backlog-list'),
+    progressListEl: document.getElementById('progress-list'),
+    completeListEl: document.getElementById('complete-list'),
+    onHoldListEl: document.getElementById('on-hold-list'),
+};
+// global variables
+// functions
+// event listeners
--- drag-and-drop/app.ts
+++ drag-and-drop/app.ts
@@ -1,10 +1,19 @@
-const addBtns = document.querySelectorAll('.add-btn:not(.solid)');
-const saveItemBtns = document.querySelectorAll('.solid');
-const addItemContainers = document.querySelectorAll('.add-container');
-const addItems = document.querySelectorAll('.add-item');
-// Item Lists
-const listColumns = document.querySelectorAll('.drag-item-list');
-const backlogListEl = document.getElementById('backlog-list');
-const progressListEl = document.getElementById('progress-list');
-const completeListEl = document.getElementById('complete-list');
-const onHoldListEl = document.getElementById('on-hold-list');
+// dom elements
+const elements = {
+  addBtns: document.querySelectorAll('.add-btn:not(.solid)'),
+  saveItemBtns: document.querySelectorAll('.solid'),
+  addItemContainers: document.querySelectorAll('.add-container'),
+  addItems: document.querySelectorAll('.add-item'),
+  // Item Lists
+  listColumns: document.querySelectorAll('.drag-item-list'),
+  backlogListEl: document.getElementById('backlog-list'),
+  progressListEl: document.getElementById('progress-list'),
+  completeListEl: document.getElementById('complete-list'),
+  onHoldListEl: document.getElementById('on-hold-list'),
+};
+
+// global variables
+
+// functions
+
+// event listeners
