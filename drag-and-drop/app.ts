073db442c9d0117104672f1d0ff5e49db3d03aa3
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-03 19:36:25 +0600

    all

--- drag-and-drop/app.ts
+++ drag-and-drop/app.ts
@@ -1,15 +1,25 @@
 // dom elements
 const elements = {
-  addBtns: document.querySelectorAll('.add-btn:not(.solid)'),
-  saveItemBtns: document.querySelectorAll('.solid'),
-  addItemContainers: document.querySelectorAll('.add-container'),
-  addItems: document.querySelectorAll('.add-item'),
+  addBtns: document.querySelectorAll(
+    '.add-btn:not(.solid)'
+  ) as NodeListOf<HTMLButtonElement>,
+  saveItemBtns: document.querySelectorAll(
+    '.solid'
+  ) as NodeListOf<HTMLButtonElement>,
+  addItemContainers: document.querySelectorAll(
+    '.add-container'
+  ) as NodeListOf<HTMLDivElement>,
+  addItems: document.querySelectorAll(
+    '.add-item'
+  ) as NodeListOf<HTMLDivElement>,
   // Item Lists
-  listColumns: document.querySelectorAll('.drag-item-list'),
-  backlogListEl: document.getElementById('backlog-list'),
-  progressListEl: document.getElementById('progress-list'),
-  completeListEl: document.getElementById('complete-list'),
-  onHoldListEl: document.getElementById('on-hold-list'),
+  listColumns: document.querySelectorAll(
+    '.drag-item-list'
+  ) as NodeListOf<HTMLUListElement>,
+  backlogListEl: document.getElementById('backlog-list') as HTMLUListElement,
+  progressListEl: document.getElementById('progress-list') as HTMLUListElement,
+  completeListEl: document.getElementById('complete-list') as HTMLUListElement,
+  onHoldListEl: document.getElementById('on-hold-list') as HTMLUListElement,
 };
 
 // global variables
