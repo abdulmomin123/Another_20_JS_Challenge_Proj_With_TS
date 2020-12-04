b4ef8e09c29b9c8c989ac0069f1d461604f5d398
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-04 20:30:06 +0600

    all

---
+++ calculator/app.js
@@ -0,0 +1,10 @@
+"use strict";
+// elements
+const elements = {
+    calculatorDisplay: document.querySelector('h1'),
+    inputBtns: document.querySelectorAll('button'),
+    clearBtn: document.getElementById('clear-btn'),
+};
+// global variables
+// functions
+// event listeners
---
+++ calculator/app.ts
@@ -0,0 +1,14 @@
+// elements
+const elements = {
+  calculatorDisplay: document.querySelector('h1') as HTMLHeadingElement,
+  inputBtns: document.querySelectorAll(
+    'button'
+  ) as NodeListOf<HTMLButtonElement>,
+  clearBtn: document.getElementById('clear-btn') as HTMLButtonElement,
+};
+
+// global variables
+
+// functions
+
+// event listeners
