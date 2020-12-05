afc74370cd482239a2ee6c2caef5b2cc48b9e658
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-05 21:01:13 +0600

    all

--- calculator/app.js
+++ calculator/app.js
@@ -3,8 +3,28 @@
 const elements = {
     calculatorDisplay: document.querySelector('h1'),
     inputBtns: document.querySelectorAll('button'),
+    btnsContainer: document.querySelector('.calculator-buttons'),
     clearBtn: document.getElementById('clear-btn'),
 };
 // global variables
 // functions
+const add = () => {
+    //
+};
+const subtract = () => {
+    //
+};
+const multiply = () => {
+    //
+};
+const divide = () => {
+    //
+};
 // event listeners
+// click handler on any button
+elements.btnsContainer.addEventListener('click', e => {
+    const target = e.target;
+    if (target.classList.contains('calculator-buttons'))
+        return;
+    console.log(target);
+});
--- calculator/app.ts
+++ calculator/app.ts
@@ -4,11 +4,37 @@ const elements = {
   inputBtns: document.querySelectorAll(
     'button'
   ) as NodeListOf<HTMLButtonElement>,
+  btnsContainer: document.querySelector(
+    '.calculator-buttons'
+  ) as HTMLDivElement,
   clearBtn: document.getElementById('clear-btn') as HTMLButtonElement,
 };
 
 // global variables
 
 // functions
+const add = () => {
+  //
+};
+
+const subtract = () => {
+  //
+};
+
+const multiply = () => {
+  //
+};
+
+const divide = () => {
+  //
+};
 
 // event listeners
+// click handler on any button
+elements.btnsContainer.addEventListener('click', e => {
+  const target = e.target as HTMLButtonElement;
+
+  if (target.classList.contains('calculator-buttons')) return;
+
+  console.log(target);
+});
