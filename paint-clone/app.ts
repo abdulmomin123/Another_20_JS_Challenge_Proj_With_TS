3b45eaf9e81c5e586ca28422f504a13cbc3c2cca
Abdul Momin <abdulmomin114477@gmail.com>
2020-12-10 23:29:37 +0600

    all

--- paint-clone/app.ts
+++ paint-clone/app.ts
@@ -1,16 +1,18 @@
-const activeToolEl = document.getElementById('active-tool');
-const brushColorBtn = document.getElementById('brush-color');
-const brushIcon = document.getElementById('brush');
-const brushSize = document.getElementById('brush-size');
-const brushSlider = document.getElementById('brush-slider');
-const bucketColorBtn = document.getElementById('bucket-color');
-const eraser = document.getElementById('eraser');
-const clearCanvasBtn = document.getElementById('clear-canvas');
-const saveStorageBtn = document.getElementById('save-storage');
-const loadStorageBtn = document.getElementById('load-storage');
-const clearStorageBtn = document.getElementById('clear-storage');
-const downloadBtn = document.getElementById('download');
-const { body } = document;
+// dom elements
+const elements = {
+  activeToolEl: document.getElementById('active-tool'),
+  brushColorBtn: document.getElementById('brush-color'),
+  brushIcon: document.getElementById('brush'),
+  brushSize: document.getElementById('brush-size'),
+  brushSlider: document.getElementById('brush-slider'),
+  bucketColorBtn: document.getElementById('bucket-color'),
+  eraser: document.getElementById('eraser'),
+  clearCanvasBtn: document.getElementById('clear-canvas'),
+  saveStorageBtn: document.getElementById('save-storage'),
+  loadStorageBtn: document.getElementById('load-storage'),
+  clearStorageBtn: document.getElementById('clear-storage'),
+  downloadBtn: document.getElementById('download'),
+};
 
 // Global Variables
 
